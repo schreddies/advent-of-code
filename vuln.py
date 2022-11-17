0cbd697926050ccc3232a3cfce77fa65bdb2d1b1
Artur Balsam <artur.balsam@hotmail.com>
2022-11-17 14:03:16 +0100

    Create vuln.py

---
+++ vuln.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def index(request):
+    value = request.GET.get("value")
+    response = HttpResponse("")
+    response["Set-Cookie"] = value  # Noncompliant
+    response.set_cookie("sessionid", value)  # Noncompliant
+    return response
